c2fbbe02e669d79ffac9de5f3c4836df1a2eb035
manucarenjot <carenjotje@gmail.com>
2021-11-29 13:34:10 +0100

    Génèrateur d'histoire

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,13 @@
-const nom = document.getElementById('champs');
+const champs = document.getElementById('champs');
 const generateur = document.getElementsByTagName('button');
 
 
-let
+let nom = ['jocelyn', 'Jackie', 'Django', 'Paprika', 'Ada'];
+let objet = ['crayon', 'souris', 'ordinateur', 'batte de baseball', 'sac à dos'];
+let tempèratures = ['-8000°', '7°', '59°', '1°', '-15°'];
+let lieux = ['le kebab de Jackie', 'le Groenland', 'Ogoudou', 'Bourg Palestine', 'Zereth Mortis'];
+let verbes = ['courir', 'parler', 'utiliser', 'frapper', 'Nager'];
+
+
+
+

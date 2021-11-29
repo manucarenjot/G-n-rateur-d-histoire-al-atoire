2700d26e7f4c0fc0da6343055aedf4676f0972d4
manucarenjot <carenjotje@gmail.com>
2021-11-29 19:03:41 +0100

    generateur

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,12 @@ const history = document.getElementById('history');
 
 
 
-let nom = ['jocelyn', 'Jackie', 'Django', 'Paprika', 'Ada'];
-let objet = ['crayon', 'souris', 'ordinateur', 'batte de baseball', 'sac à dos'];
-let verbes = ['ai', 'rentre', 'fait', 'cour', 'parle', 'utilise', 'frappe', 'Nage'];
+let nom = ['jocelyn', 'Jackie', 'Django', 'Paprika', 'Eric Zemmour'];
+let nom2 = ['Ada', 'Jean-Marie Bigard', 'pikachu']
+let objet = ['un crayon', 'une souris', 'un ordinateur', 'une batte de baseball', ' un sac à dos', 'une raclette'];
+let verbes = ['ont', 'rentre', 'font', 'cours', 'parle', 'utilisent', 'frappes', 'Nagent'];
 let temperatures = ['-8000°', '7°', '59°', '1°', '-15°'];
-let lieux = ['le kebab de Jackie ', ' Groenland', 'Ogoudou', 'Bourg Palestine', 'Zereth Mortis'];
+let lieux = [' Yaoundé ', ' Groenland', 'Ogoudou', 'Bourg Palestine', 'Zereth Mortis'];
 
 
 
@@ -17,20 +18,20 @@ let lieux = ['le kebab de Jackie ', ' Groenland', 'Ogoudou', 'Bourg Palestine', 
 function generer(){
     const newHistory = document.createElement('p');
     history.appendChild(newHistory);
-    let randomNom = Math.round(Math.random() * nom.length );
+    let randomNom = Math.round(Math.random() * nom.length);
+    let randomNom2 = Math.round(Math.random() * nom2.length);
     let randomObjet = Math.round(Math.random() * objet.length);
-    console.log(randomObjet);
     let randomVerbes = Math.round(Math.random() * verbes.length);
     let randomTemperatures = Math.round(Math.random()* temperatures.length);
     let randomLieux =  Math.round(Math.random()* lieux.length);
 
 
-    newHistory.innerHTML = nom[randomNom] + ' ' + verbes[randomVerbes] + ' ' + objet[randomObjet] + ' ' + temperatures[randomTemperatures] + ' ' + lieux[randomLieux]+ ' ' + verbes[randomVerbes];
+
+    newHistory.innerHTML =   nom[randomNom] + ' et '+ nom2[randomNom] + ' '+ verbes[randomVerbes] + ' avec ' + objet[randomObjet] + ' alors qu il fait ' + temperatures[randomTemperatures] + ' à ' + lieux[randomLieux]+ ' ' + champs.value;
 }
 
 generateur.addEventListener("click", function (e){
     generer()
-    console.log(champs.value)
 })
 
 

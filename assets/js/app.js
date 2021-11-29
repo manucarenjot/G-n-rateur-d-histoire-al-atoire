21954976b08be67a849a935b7ff7f637372aecb4
manucarenjot <carenjotje@gmail.com>
2021-11-29 14:46:52 +0100

    Génèrateur d'histoire

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,13 @@ const champs = document.getElementById('champs');
 const generateur = document.querySelector('button');
 const history = document.getElementById('history');
 
+console.log(champs.value)
 
 let nom = ['jocelyn', 'Jackie', 'Django', 'Paprika', 'Ada'];
 let objet = ['crayon', 'souris', 'ordinateur', 'batte de baseball', 'sac à dos'];
 let verbes = ['ai', 'rentre', 'fait', 'cour', 'parle', 'utilise', 'frappe', 'Nage'];
 let temperatures = ['-8000°', '7°', '59°', '1°', '-15°'];
-let lieux = ['le kebab de Jackie', ' Groenland', 'Ogoudou', 'Bourg Palestine', 'Zereth Mortis'];
+let lieux = ['le kebab de Jackie ', ' Groenland', 'Ogoudou', 'Bourg Palestine', 'Zereth Mortis'];
 
 
 
@@ -17,8 +18,14 @@ function generer(){
     const newHistory = document.createElement('p');
     history.appendChild(newHistory);
     let randomNom = Math.round(Math.random() * nom.length );
+    let randomObjet = Math.round(Math.random() * objet.length);
+    console.log(randomObjet);
+    let randomVerbes = Math.round(Math.random() * verbes.length);
+    let randomTemperatures = Math.round(Math.random()* temperatures.length);
+    let randomLieux =  Math.round(Math.random()* lieux.length);
 
-    newHistory.innerHTML = nom[randomNom] + '' + objet[rand2]+ '' + temperatures[rand2]+ '' + lieux[rand2]+ '' + verbes[rand2];
+
+    newHistory.innerHTML = nom[randomNom] + ' ' + verbes[randomVerbes] + ' ' + objet[randomObjet] + ' ' + temperatures[randomTemperatures] + ' ' + lieux[randomLieux]+ ' ' + verbes[randomVerbes];
 }
 
 generateur.addEventListener("click", function (e){
